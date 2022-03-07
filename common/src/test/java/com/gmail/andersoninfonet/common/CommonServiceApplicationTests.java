e9fbb3a8f4864a830642fc32bb6bef4a8d36c019
Anderson Dias <andersoninfonet@gmail.com>
2022-03-06 22:58:46 -0300

    create the projects

---
+++ common/src/test/java/com/gmail/andersoninfonet/common/CommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.andersoninfonet.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment/src/test/java/com/gmail/andersoninfonet/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.andersoninfonet.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
